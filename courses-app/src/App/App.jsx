cb2f4d5c9ceb262ec8d65071c659609a682cb2f1
Volha Hrakholskaya <volha_hrakholskaya@epam.com>
2021-05-06 08:09:34 +0300

    feat: removed fetch, connected with store

--- courses-app/src/App/App.jsx
+++ courses-app/src/App/App.jsx
@@ -4,25 +4,27 @@ import {
   Route,
   Switch
 } from "react-router-dom";
-import axios from 'axios';
+import { connect, useDispatch } from "react-redux";
+import PropTypes from 'prop-types';
+
+import { getCourses } from "store/courses/actionCreators";
+import { getAuthors } from "store/author/actionCreators";
 
 import Header from "components/Header/Header";
 import Courses from "pages/Courses/Courses";
-import { Login } from "pages/Login/Login";
+import Login from "pages/Login/Login";
 import { Registration } from "pages/Registration/Registration";
-import { CourseInfo } from "components/CourseInfo/CourseInfo";
-
-import { NewCourseForm } from "pages/NewCourseForm/NewCourseForm";
-// import { mockedCourseList, mockedAddAuthor } from "localService/Mock";
+import CourseInfo from "components/CourseInfo/CourseInfo";
+import CreateCourse from "pages/CreateCourse/CreateCourse";
 
 import './App.css';
 
-function App() {
-  const [user, setUser] = useState();
+const App = (props) => {
+  const { isAuth } = props;
+  const [user, setUser] = useState({isAuth: false, email: null});
   const [isLogin, setLogin] = useState(false);
-  // const [isRegistered, setRegistered] = useState(false);
-  const [courses, setCourses] = useState([]);
   const [authors, setAuthors] = useState([]);
+  const dispathc = useDispatch();
 
   useEffect(() => {
     const currentUser = window.localStorage.getItem("currentUser");
@@ -31,57 +33,12 @@ function App() {
       setUser(JSON.parse(currentUser));
       token ? setLogin(true) : setLogin(false);
     }
-    const registered = window.localStorage.getItem("registered");
-    console.log(registered)
   }, []);
 
-  useEffect(() => {
-    const fetchCourses = async () => {
-      const options = {
-          method: 'GET',
-          headers: { 
-              'accept': '*/*'
-          },
-          url: `http://localhost:3000/courses/all`
-      };
-      try {
-        const result = await axios(options);
-        setCourses(result.data.result);
-      } catch(error) {
-        alert(error.message)
-      }
-    }
-    fetchCourses();
-  }, []);
-
-  useEffect(() => {
-    const fetchAuthors = async () => {
-      const options = {
-          method: 'GET',
-          headers: { 
-              'accept': '*/*'
-          },
-          url: `http://localhost:3000/authors/all`
-      };
-      try {
-        const result = await axios(options);
-        setAuthors(result.data.result)
-        console.log("FETCH RESULT", result)
-      } catch(error) {
-        console.log("ERROR", error)
-      }
-    }
-    fetchAuthors();
-  }, []);
-
-  function handleAddCourse(newCourse) {
-    setCourses([...courses, newCourse])
-  }
-
-  function handleChangeUser(newUser) {
-    setUser(newUser);
-    setLogin(true);
-  }
+  useEffect(()=> {
+    dispathc(getCourses())
+    dispathc(getAuthors())
+  }, [])
 
   function handleAddAuthors(newAuthor) {
     setAuthors([...authors, newAuthor])
@@ -89,30 +46,33 @@ function App() {
 
   return (
     <>
-      <Header isLogin={isLogin} user={user} />
+      <Header user={user}/>
         <Switch>
           <Route path="/courses/add">
-            <NewCourseForm 
-                onAddCourse={handleAddCourse}
+            <CreateCourse 
+                // onAddCourse={handleAddCourse}
                 onAddAuthor={handleAddAuthors}/>
           </Route>
-          <Route path="/courses/:courseId">
-            <CourseInfo courses={courses} authorsList={authors}/>
-          </Route>
-          <Route path="/courses">
-            <Courses courses={courses} authors={authors}/>
-          </Route>
-          <Route path="/login">
-            <Login onChangeUser={handleChangeUser}/>
-          </Route>
-          <Route path="/registration">
-            <Registration />
-          </Route>
+          <Route path="/courses/:courseId" component={CourseInfo}/>
+          <Route path="/courses" component={Courses}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/registration" component={Registration}/>
         </Switch>
+        {isLogin ? <Redirect exact to="/courses"/> : <Redirect exact to="/login"/> }
+        {isLogin ? console.log('ISLOGIN') : console.log('NOTLOGIN') }
         <Redirect from="/" to="/courses"/>
-        {isLogin ? <Redirect to="/courses"/> : <Redirect to="/login"/> }
     </>
   );
 }
 
-export default App;
+App.propTypes = {
+  getCourses: PropTypes.func,
+  isAuth: PropTypes.bool
+}
+
+function mapStateToProps(state) {
+    const user = state.userReducer
+    return user
+}
+
+export default connect(mapStateToProps, {getCourses, getAuthors})(App);
