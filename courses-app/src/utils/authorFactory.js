d0d12eb76805785e1b05d86a44f75c1c342c86b2
Volha Hrakholskaya <volha_hrakholskaya@epam.com>
2021-05-12 20:55:05 +0300

    delete: removed author factory

--- courses-app/src/utils/authorFactory.js
+++
@@ -1,8 +0,0 @@
-import uuid from 'react-uuid';
-
-export function authorFactory(authorName) {
-    let author = new Object();
-    author.id = uuid();
-    author.name = authorName;
-    return author;
-}
--- courses-app/src/utils/index.js
+++ courses-app/src/utils/index.js
@@ -1,4 +1,3 @@
 export * from "./formatter";
-export * from "./authorFactory";
 export * from "./validation";
 export * from "./getAuthorsId";
