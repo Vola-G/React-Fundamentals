b7d6bdf5e87f722e3915274eca0e3c313dc7895b
Volha Hrakholskaya <volha_hrakholskaya@epam.com>
2021-05-13 23:06:16 +0300

    feat: added new author validation

--- courses-app/src/components/CreateNewAuthor/CreateNewAuthor.jsx
+++ courses-app/src/components/CreateNewAuthor/CreateNewAuthor.jsx
@@ -8,24 +8,38 @@ import Input from "../Input/Input";
 import Button from "../Button/Button";
 import { ParametersTitle } from "../ParametersTitle/ParametersTitle";
 
+import { useAuthorValidation } from "../../utils/validation";
+
 export function CreateNewAuthor() {
-    const [value, setValue] = useState("");
     const dispathc = useDispatch();
+    const [author, setAuthor] = useState("");
+    const [isAuthorValid, setAuthorValid] = useState(false);
+    const [authorWarning, setAuthorWarning] = useState("")
 
     function handleChange(newAuthor) {
-        setValue(newAuthor);
+        setAuthor(newAuthor);
+        if(useAuthorValidation(newAuthor)) {
+            setAuthorValid(true);
+            setAuthorWarning("")
+        } else {
+            setAuthorValid(false);
+            setAuthorWarning("Author name should be at least 2 characters")
+        } 
     }
 
     function handleClick() {
-        dispathc(saveAuthorsThunk(value));
-        setValue("");
+        if(isAuthorValid && author.length) {
+            dispathc(saveAuthorsThunk(author));
+            setAuthor("");
+            setAuthorWarning("")
+        }
     }
 
     return(
         <div className={"parameter-block"}>
             <ParametersTitle title="Add author" variant="h4" component="h2"/>
             <div className={"parameter-block_form"}>
-                <Input label="Author name" type="text" value={value} onChange={handleChange}/>
+                <Input label="Author name" type="text" value={author} onChange={handleChange} helperText={authorWarning}/>
                 <div style={{width: "auto", margin: "30px"}}>
                     <Button name="Create Author" variant="contained" color="primary" onClick={handleClick}/>
                 </div>
