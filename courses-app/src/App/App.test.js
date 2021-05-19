e57e88d0798826f598402ac962055e953e0ce3d8
Volha Hrakholskaya <volha_hrakholskaya@epam.com>
2021-05-19 11:20:38 +0300

    feat: added app test

--- courses-app/src/App/App.test.js
+++ courses-app/src/App/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from '../store/index';
+import { BrowserRouter } from 'react-router-dom';
+
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />      
+      </BrowserRouter>
+    </Provider>
+  );
+  expect(getByText(/Courses-App/i)).toBeInTheDocument();
 });
